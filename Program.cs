6670691d6c39cd5f508e65f232c20d3e40637180
MakerCorn <corn.mendoza@outlook.com>
2021-02-14 19:57:35 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.SpringCloud.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Steeltoe.Discovery.Client;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Logging;
 using Steeltoe.Management.CloudFoundry;
+
+#if __USE_DISCOVERY_CLIENT__
+using Steeltoe.Discovery.Client;
 using Steeltoe.Discovery.Kubernetes;
 using Steeltoe.Discovery.Eureka;
 using Steeltoe.Discovery.Consul;
+#endif
 
 namespace cms_mvc
 {
@@ -39,12 +42,14 @@ namespace cms_mvc
                 // Add Config Server if available
                 .AddConfigServer()
 
+#if __USE_DISCOVERY_CLIENT__
                 // Add Discovery Client
                 .AddDiscoveryClient()
                 .AddServiceDiscovery(options => options
                     .UseEureka()
                     .UseKubernetes()
                     .UseConsul())
+#endif
 
                 .UseStartup<Startup>();
 
